d61bd81499eca6a4fedbf1a2e7e88f42960e847f
DENGYONGZHEN <yongzhendeng111@gmail.com>
2024-08-11 17:32:12 +0900

    email confirm verification added

--- auth.ts
+++ auth.ts
@@ -26,16 +26,17 @@ export const {
     },
   },
   callbacks: {
-    // async signIn({ user }) {
-    //   const existingUser = await getUserById(user.id as string);
-    //   console.log("====================================");
-    //   console.log(existingUser);
-    //   console.log("====================================");
-    //   if (!existingUser || !existingUser.emailVerified) {
-    //     return false;
-    //   }
-    //   return true;
-    // },
+    async signIn({ user, account }) {
+      //Allow OAuth without email verification
+      if (account?.provider !== "credentials") return true;
+      const existingUser = await getUserById(user.id as string);
+      //prevent sign in without email verification
+      if (!existingUser || !existingUser?.emailVerified) {
+        return false;
+      }
+      //TODO: Add 2FA check
+      return true;
+    },
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;

875a8bdfb94b6762877843f97ab0012172397a5b
DENGYONGZHEN <yongzhendeng111@gmail.com>
2024-08-11 15:10:53 +0900

    events added

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -18,8 +18,14 @@ import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
 import { login } from "@/actions/login";
 import { useState, useTransition } from "react";
+import { useSearchParams } from "next/navigation";
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const urlError =
+    searchParams.get("error") === "OAuthAccountNotLinked"
+      ? "Email already in use with different provider"
+      : "";
   const [success, setSuccess] = useState<string | undefined>();
   const [error, setError] = useState<string | undefined>();
   const [isPending, startTransition] = useTransition();
@@ -37,6 +43,8 @@ export const LoginForm = () => {
     startTransition(() => {
       login(values).then((data) => {
         setError(data?.error);
+        //TODO:Add when we add 2FA
+        // setSuccess(data.success)
       });
       form.reset();
     });
@@ -89,7 +97,7 @@ export const LoginForm = () => {
               )}
             />
           </div>
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSuccess message={success} />
           <Button type="submit" className="w-full" disabled={isPending}>
             Login
